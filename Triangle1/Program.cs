e9f176e3ca80d6c0564dae3ec86d4e48e34cb815
MrUlkiNov <sam.baybekov@mail.ru>
2024-11-14 01:33:40 +0300

    Triangl

---
+++ Triangle1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace TriangleApp
+{
+    public enum TriangleType
+    {
+        Acute,    
+        Obtuse,   
+        Right,  
+        NotTriangle 
+    }
+
+    public class Triangle
+    {
+        public static TriangleType DetermineTriangleType(double a, double b, double c)
+        {
+            if (!IsValidTriangle(a, b, c))
+                return TriangleType.NotTriangle;
+
+            double[] sides = { a, b, c };
+            Array.Sort(sides);
+            a = sides[0];
+            b = sides[1];
+            c = sides[2];
+
+            double aSquared = a * a;
+            double bSquared = b * b;
+            double cSquared = c * c;
+
+            if (aSquared + bSquared > cSquared)
+                return TriangleType.Acute;
+            else if (aSquared + bSquared == cSquared)
+                return TriangleType.Right;
+            else
+                return TriangleType.Obtuse;
+        }
+
+        public static bool IsValidTriangle(double a, double b, double c)
+        {
+            return a > 0 && b > 0 && c > 0 &&
+                   a + b > c &&
+                   a + c > b &&
+                   b + c > a;
+        }
+
+        public static double CalculateArea(double a, double b, double c)
+        {
+            if (!IsValidTriangle(a, b, c))
+                throw new ArgumentException("The provided sides do not form a triangle.");
+
+            double semiPerimeter = (a + b + c) / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину стороны a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите длину стороны b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Введите длину стороны c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            if (!Triangle.IsValidTriangle(a, b, c))
+            {
+                Console.WriteLine("Введенные значения не образуют треугольник.");
+            }
+            else
+            {
+                var type = Triangle.DetermineTriangleType(a, b, c);
+                Console.WriteLine($"Тип треугольника: {type}");
+
+                double area = Triangle.CalculateArea(a, b, c);
+                Console.WriteLine($"Площадь треугольника: {area}");
+            }
+        }
+    }
+}
---
+++ TriangleApp.Tests/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Xunit;
+using TriangleApp;
+
+namespace TriangleApp.Tests
+{
+    public class TriangleTests
+    {
+        [Theory]
+        [InlineData(3, 4, 5, TriangleType.Right)]
+        [InlineData(5, 5, 5, TriangleType.Acute)]
+        [InlineData(2, 2, 3, TriangleType.Acute)]
+        [InlineData(7, 24, 25, TriangleType.Right)]
+        [InlineData(5, 12, 13, TriangleType.Right)]
+        [InlineData(3, 4, 6, TriangleType.Obtuse)]
+        public void DetermineTriangleType_ReturnsCorrectType(double a, double b, double c, TriangleType expected)
+        {
+            var result = Triangle.DetermineTriangleType(a, b, c);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(3, 4, 5, true)]
+        [InlineData(1, 2, 3, false)]  // Не треугольник, сумма двух сторон равна третьей
+        [InlineData(0, 4, 5, false)]  // Не треугольник, одна сторона равна 0
+        [InlineData(-3, 4, 5, false)] // Не треугольник, отрицательная сторона
+        public void IsValidTriangle_ReturnsCorrectValidation(double a, double b, double c, bool expected)
+        {
+            var result = Triangle.IsValidTriangle(a, b, c);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(3, 4, 5, 6)]
+        [InlineData(5, 5, 5, 10.825317547305486)]
+        [InlineData(7, 24, 25, 84)]
+        public void CalculateArea_ReturnsCorrectArea(double a, double b, double c, double expected)
+        {
+            var result = Triangle.CalculateArea(a, b, c);
+            Assert.Equal(expected, result, 5); // точность до 5 знаков
+        }
+
+        [Fact]
+        public void CalculateArea_WithInvalidTriangle_ThrowsException()
+        {
+            Assert.Throws<ArgumentException>(() => Triangle.CalculateArea(1, 2, 3));
+        }
+    }
+}
